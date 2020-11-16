1151aaf46d70c88dd38e2b5bb415dd1eb488f9f6
xaakko <56148622+xaakko@users.noreply.github.com>
2020-11-16 16:15:36 +0500

    Add files via upload

---
+++ your_age.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+        <body>
+        <center>
+            
+<style>
+    
+        #back {
+          height: "auto" px;
+          width: 600px;
+          background-image: linear-gradient(to right, #a826ff , #250c35);
+          color: white;
+          padding: 14px 20px;
+          margin: 8px 0;
+          box-shadow: 0 5px 15px 0 rgba(10, 36, 19, 0.2), 0 2px 15px 0 rgba(0, 0, 0, 0.30);
+          border-style: solid;
+          border-width: 5px;
+          border-radius: 25px;
+        }
+        
+        input {
+                            color: #250c35;
+                            background-color: #ffff;
+                            background-size: 400% 400%;
+                            -webkit-animation: gradientBG 10s ease infinite;
+                            animation: gradientBG 10s ease infinite;
+                            border: none;
+                            color: #250c35;
+                            padding: 15px 32px;
+                            text-align: center;
+                            border-radius: 25px;
+                            text-decoration: none;
+                            display: inline-block;
+                            margin: 0;
+                            font-size: 16px;
+                            -webkit-animation: gradientBG 10s ease infinite;
+                            animation: gradientBG 10s ease infinite;
+                          }
+
+        </style>
+
+<div id="back">
+
+<h1>: އުމުރަކީ</h1>
+
+
+<?php
+
+$birthyear =$_GET ['byear'];
+$currentyear =$_GET ['cyear'];
+
+$your_age_is =$currentyear - $birthyear;
+
+
+echo $your_age_is;
+
+
+
+
+?>
+<style>
+          @font-face {
+            font-family: myFirstFont;
+            src: url(mvtyper.woff);
+         }
+         
+         * {
+            font-family: myFirstFont;
+         }
+        </style>
+
+        <dir='rtl'>
+</lang='DV'>
+<br>
+        </br>
+<form action="https://www.naares.com/">
+                            <input type="submit" value="ނާރެސް އަށް ދިއުމަށް" />
+                        </form>
+                        <form action="https://www.naares.com/">
+                            <input type="submit" value="! ފައިލްތަށް ޑައުންލޯޑް ކުރޭ" />
+                        </form>
+
+</center>
+</div>
+</body>
+</head>
+</html>
